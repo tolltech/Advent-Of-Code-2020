3abdfc44492a5ec185cd86bf25394360c4c22082
tolltech <alexandrovpe@gmail.com>
2020-11-30 12:14:17 +0400

    init

---
+++ AoC_2020/Task01.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2020
+{
+    [TestFixture]
+    public class Task01
+    {
+        [Test]
+        [TestCase(1, 2, 3)]
+        [TestCase(1, 3, 4)]
+        [TestCase(1, 3, 5)]
+        public void Task(int a, int b, int expected)
+        {
+            (a + b).Should().Be(expected);
+        }
+    }
+}
